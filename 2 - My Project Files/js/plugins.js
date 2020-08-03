8d835400493a912f15ead7d445d4b454e1737ff0
ipkhovelishvili <ipkhovelishvili@riotgames.com>
2020-08-03 15:57:41 +0800

    added navbar solid toggle on click

--- 2 - My Project Files/js/plugins.js
+++ 2 - My Project Files/js/plugins.js
@@ -1,17 +1,24 @@
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
-$(document).ready(function () {
-    $(window).scroll(checkScroll);
-    function checkScroll() {
-        if ($(window).scrollTop() >= 300) {
-            $('.navbar').addClass('solid');
-        } else {
-            $('.navbar').removeClass('solid');
-        }
-    }
-})
+$(document).ready(function () { //when document(DOM) loads completely
+    checkScroll(); //check if page is scrolled
+    $(window).scroll(checkScroll); //get scroll position of window
+});
+
+function checkScroll() { //check if page is scrolled
+if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
+    $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
+} else { //if page is not scrolled 300px from top
+    $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
+}
+}
+
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
-
+$('.navbar-toggler').click(function () { //when navbar-toggler is clicked
+    if ($(window).scrollTop() <= 300) { //if window scrolled 300px or less from top
+    $("nav.navbar").toggleClass("solid-toggle"); //add the solid class to navbar
+    }
+});
 
 /*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
 
