86741d586a32d88c37bf751cf62b18a4ed35248e
ipkhovelishvili <ipkhovelishvili@riotgames.com>
2020-08-10 18:11:34 +0800

    added meet the team carousel section

--- 2 - My Project Files/js/plugins.js
+++ 2 - My Project Files/js/plugins.js
@@ -52,7 +52,27 @@ $(document).ready(function () {
 });
 
 /*========== MEET THE TEAM CAROUSEL ==========*/
-
+$(document).ready(function(){ //when document(DOM) loads completely
+    $('#team-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: false, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive : { //set number of items shown per screen width
+            0 : {
+                items: 1 //0px width and up display 1 item
+            },
+            768 : {
+                items: 2 //768px width and up display 2 items
+            },
+			992 : {
+                items: 3 //992px width and up display 2 items
+            }
+        }
+    });
+  });
 
 /*========== SKILLS COUNTER ==========*/
 
